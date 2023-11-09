11401342852e98bee95811e6cf493a0e767f6104
Arachneee <jhg2819@naver.com>
2023-11-10 00:11:45 +0900

    refactor: enum 값 재설정
    Star enum 제거 후 요일 계산 enum 추가

---
+++ src/main/java/christmas/constant/DayOfWeek.java
@@ -0,0 +1,50 @@
+package christmas.constant;
+
+import static christmas.constant.Week.WEEKDAY;
+import static christmas.constant.Week.WEEKEND;
+
+import christmas.domain.order.Day;
+import christmas.exception.ErrorMessage;
+import christmas.exception.OrderException;
+import java.util.Arrays;
+
+public enum DayOfWeek {
+    FRIDAY(1, WEEKEND),
+    SATURDAY(2, WEEKEND),
+    SUNDAY(3, WEEKDAY),
+    MONDAY(4, WEEKDAY),
+    TUESDAY(5, WEEKDAY),
+    WEDNESDAY(6, WEEKDAY),
+    THURSDAY(0, WEEKDAY);
+
+    private final int modSevenNumber;
+    private final Week week;
+
+    DayOfWeek(final int modSevenNumber, final Week week) {
+        this.modSevenNumber = modSevenNumber;
+        this.week = week;
+    }
+
+    public static DayOfWeek from(final Day day) {
+        return Arrays.stream(values())
+                .filter(dayOfWeek -> isModNumber(day, dayOfWeek))
+                .findFirst()
+                .orElseThrow(() -> OrderException.from(ErrorMessage.INVALID_DAY));
+    }
+
+    private static boolean isModNumber(final Day day, final DayOfWeek dayOfWeek) {
+        return dayOfWeek.modSevenNumber == day.mod(totalCount());
+    }
+
+    private static int totalCount() {
+        return (int) Arrays.stream(values()).count();
+    }
+
+    public boolean isSunDay() {
+        return this.equals(SUNDAY);
+    }
+
+    public boolean isWeekend() {
+        return this.week.equals(WEEKEND);
+    }
+}
---
+++ src/main/java/christmas/constant/December.java
@@ -0,0 +1,25 @@
+package christmas.constant;
+
+public enum December {
+    START_DAY(1),
+    CHRISTMAS_DAY(25),
+    END_DAY(31);
+
+    private final int day;
+
+    December(final int day) {
+        this.day = day;
+    }
+
+    public static boolean in(final int day) {
+        return day >= START_DAY.getDay() && day <= END_DAY.getDay();
+    }
+
+    public static boolean isChristMas(final int day) {
+        return day == CHRISTMAS_DAY.getDay();
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
