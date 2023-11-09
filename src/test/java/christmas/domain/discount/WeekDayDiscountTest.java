c7c3bcd463ad1454ebecdc3ab2f11486a230ce3c
Arachneee <jhg2819@naver.com>
2023-11-09 18:20:58 +0900

    test: 평일에 디저트 메뉴 1개당 2023원 할인 합계를 계산할 수 있다.

---
+++ src/test/java/christmas/domain/discount/WeekDayDiscountTest.java
@@ -0,0 +1,46 @@
+package christmas.domain.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import christmas.constant.Menu;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderDay;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class WeekDayDiscountTest {
+
+    @DisplayName("평일에 디저트 메뉴 1개당 2023원 할인 합계를 계산할 수 있다.")
+    @ParameterizedTest
+    @MethodSource("weekDayMenuProvider")
+    void calculateAmount(Map<Menu, Integer> menuCount, int discountTarget) {
+        // given
+        WeekDayDiscount weekDayDiscount = new WeekDayDiscount();
+        OrderDay orderDay = OrderDay.from(1);
+        Order order = Order.of(orderDay, menuCount);
+
+        // when
+        int discountAmount = weekDayDiscount.calculateAmount(order);
+
+        // then
+        assertThat(discountAmount).isEqualTo(discountTarget);
+
+    }
+
+    static Stream<Arguments> weekDayMenuProvider() {
+        return Stream.of(
+                arguments(Map.of(Menu.ICE_CREAM, 1), 2023),
+                arguments(Map.of(Menu.ICE_CREAM, 2), 2023 * 2),
+                arguments(Map.of(Menu.TAPAS, 1), 0),
+                arguments(Map.of(Menu.CAESAR_SALAD, 1, Menu.CHOCOLATE_CAKE, 10), 2023 * 10),
+                arguments(Map.of(Menu.ICE_CREAM, 3, Menu.CHOCOLATE_CAKE, 5), 2023 * 8),
+                arguments(Map.of(Menu.BUTTON_MUSHROOM_SOUP, 1, Menu.ZERO_COLA, 1), 0)
+        );
+    }
+
+}
