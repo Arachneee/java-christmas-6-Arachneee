c72e102b27ec38a19cc11df80e30dd24089598f9
Arachneee <jhg2819@naver.com>
2023-11-09 20:03:17 +0900

    test: rename Special -> Star

--- src/test/java/christmas/domain/discount/StarDiscountTest.java
+++ src/test/java/christmas/domain/discount/StarDiscountTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class SpecialDiscountTest {
+class StarDiscountTest {
 
     SpecialDiscount specialDiscount = new SpecialDiscount();
 
