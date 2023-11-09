52eb1339aaca954d3384ea87b5d26c3b52a49e99
Arachneee <jhg2819@naver.com>
2023-11-10 01:15:22 +0900

    feat: 배지 등급 계산 기능
    enum을 활용하여 배지 등급을 계산합니다.

---
+++ src/main/java/christmas/constant/Badge.java
@@ -0,0 +1,30 @@
+package christmas.constant;
+
+import java.util.Arrays;
+
+public enum Badge {
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE("없음", 0);
+
+    private final String title;
+    private final int minPrice;
+
+    Badge(final String title, final int minPrice) {
+        this.title = title;
+        this.minPrice = minPrice;
+    }
+
+    public static Badge from(final int totalDiscount) {
+        return Arrays.stream(values())
+                .sorted()
+                .filter(entry -> entry.minPrice <= totalDiscount)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
