fbe81d8dfd2443f83de66d82ddbe052f0bfa22d3
Arachneee <jhg2819@naver.com>
2023-11-10 22:14:17 +0900

    test: category 테스트 추가

---
+++ src/test/java/christmas/domain/order/constant/CategoryTest.java
@@ -0,0 +1,26 @@
+package christmas.domain.order.constant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CategoryTest {
+
+    @DisplayName("음료가 아닌지 확인할 수 있다. True")
+    @ParameterizedTest
+    @ValueSource(strings = {"APPETIZER", "MAIN", "DESSERT"})
+    void isNotBeverageTrue(Category category) {
+
+        assertThat(category.isNotBeverage()).isTrue();
+    }
+
+    @DisplayName("음료가 아닌지 확인할 수 있다. False")
+    @Test
+    void isNotBeverageFalse() {
+
+        assertThat(Category.BEVERAGE.isNotBeverage()).isFalse();
+    }
+}
