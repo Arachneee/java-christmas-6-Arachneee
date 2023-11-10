8a091b4f783f8d0a5fe64d6fb9c0bca74e9ada8f
Arachneee <jhg2819@naver.com>
2023-11-10 13:28:50 +0900

    feat: Planner 시작 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.controller.Planner;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Planner planner = new Planner(new InputView(), new OutputView());
+        planner.run();
     }
 }
